b3c5cbe06f66f7fed913fe4608e83f31eac66b34
dhs02 <59893400+dhs02@users.noreply.github.com>
2020-03-11 09:28:30 +0100

    Qien Applicatie

---
+++ src/main/java/QienApp/qien/QienApplication.java
@@ -0,0 +1,13 @@
+package QienApp.qien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QienApplication.class, args);
+	}
+
+}
---
+++ src/test/java/QienApp/qien/QienApplicationTests.java
@@ -0,0 +1,13 @@
+package QienApp.qien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
