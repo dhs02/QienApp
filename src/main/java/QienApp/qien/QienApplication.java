a341a9c40671d89f4c4f9b74e002c41705dbf2fb
dhs02 <59893400+dhs02@users.noreply.github.com>
2020-03-20 10:07:05 +0100

    nieuwste versie / Swagger2 toegevoegd

--- src/main/java/QienApp/qien/QienApplication.java
+++ src/main/java/QienApp/qien/QienApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QienApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(QienApplication.class, args);
 	}
