7481364ed18e89a244e6eaf775d6a0996815c7b5
mauricedibbets1986 <mauricedibbets1986@gmail.com>
2020-03-20 09:58:42 +0100

    QienApp branche featureMaurice11

--- src/main/java/QienApp/qien/domein/Medewerker.java
+++ src/main/java/QienApp/qien/domein/Medewerker.java
@@ -1,10 +1,12 @@
 package QienApp.qien.domein;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
 public class Medewerker extends Gebruiker {
 	@ManyToOne
+	@JoinColumn(name = "opdrachtgever_id")
 	Opdrachtgever opdrachtgever;
 	
 	public Opdrachtgever getOpdrachtgever() {
